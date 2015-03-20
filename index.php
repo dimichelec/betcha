45eb17451755b9be42c8193b6a99a79642c4bb3a
dimichelec <dimichelec@users.noreply.github.com>
2015-03-20 11:05:26 -0400

    Update index.php

--- index.php
+++ index.php
@@ -11,46 +11,16 @@
 
   <body>
     <script>
-      var timeVar = setInterval(function () { timeFunc(); }, 1000);
       var userData;
       var searchOK = false;
       var fbOK = false;
       
-      function timeFunc() {
-        var d = new Date();
-        document.getElementById("time-span").innerHTML = d.toLocaleTimeString();
-      }
-
-      google.load('search', '1', {'callback' : searchLoaded});
-      function searchLoaded() {
+      //google.load('search', '1', {'callback' : searchLoaded});
+      google.load('search', '1', {'callback' : function() {
         searchOK = true;
         if( fbOK ) loaded();
-      }
-      
-      function loaded() {
-        var blk = document.getElementById('fb-welcome');
-        blk.innerHTML = '<table><tr>'
-          + '<td style="width:130px;"><img src="' + userData.picture.data.url + '"></td>'
-          + '<td style="vertical-align:top;">'
-            + '<b>' + userData.name + '</b><br>'
-            + '#' + userData.id + '<br>'
-            + userData.email + '<br>'
-            + userData.gender + ' over ' + (userData.age_range.min - 1) + '<br>'
-            + '<b><span style="color:' + (userData.verified ? 'green">' : 'red">not ' ) + 'verified</span></b><br>'
-            + '<span id="time-span" align="center"></span>'
-          + '</td>'
-          + '</tr></table>'
-          + '<ul>'
-          + '<li>locale: ' + userData.locale + '</li>'
-          + '<li>timezone: ' + userData.timezone + '</li>'
-          + '</ul>';
+      }});
 
-        var ctl = new google.search.SearchControl();
-        ctl.addSearcher(new google.search.PatentSearch());
-        ctl.draw(document.getElementById("searchControl"));
-        ctl.execute(userData.name);
-      }
-      
       window.fbAsyncInit = function() {
   
         FB.init({
@@ -86,8 +56,6 @@
             }, {scope: 'user_friends, email'});
           }
         });
-        
-        
       };
       
       (function(d, s, id){
@@ -97,7 +65,38 @@
          js.src = "//connect.facebook.net/en_US/sdk.js";
          fjs.parentNode.insertBefore(js, fjs);
        }(document, 'script', 'facebook-jssdk'));
-    
+
+
+      function loaded() {
+        var blk = document.getElementById('fb-welcome');
+        blk.innerHTML = '<table><tr>'
+          + '<td style="width:130px;"><img src="' + userData.picture.data.url + '"></td>'
+          + '<td style="vertical-align:top;">'
+            + '<b>' + userData.name + '</b><br>'
+            + '#' + userData.id + '<br>'
+            + userData.email + '<br>'
+            + userData.gender + ' over ' + (userData.age_range.min - 1) + '<br>'
+            + '<b><span style="color:' + (userData.verified ? 'green">' : 'red">not ' ) + 'verified</span></b><br>'
+            + '<span id="time-span" align="center"></span>'
+          + '</td>'
+          + '</tr></table>'
+          + '<ul>'
+          + '<li>locale: ' + userData.locale + '</li>'
+          + '<li>timezone: ' + userData.timezone + '</li>'
+          + '</ul>';
+
+        var ctl = new google.search.SearchControl();
+        ctl.addSearcher(new google.search.PatentSearch());
+        ctl.draw(document.getElementById("searchControl"));
+        ctl.execute(userData.name);
+      }
+      
+
+      var timeVar = setInterval(function () {
+        var d = new Date();
+        document.getElementById("time-span").innerHTML = d.toLocaleTimeString();
+      }, 1000);
+
     </script>
   
     <div id="fb-root" style="background:beige;">
